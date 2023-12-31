5fccfe250687836a85adb63307fc4c765814d3a8
pratishu <pratishu.kr@gmail.com>
2023-12-31 16:09:46 +0530

    font setup with tailwind - roboto and poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,27 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import { Poppins } from "next/font/google";
+import { Roboto } from "next/font/google";
+import { Inter } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
+const inter = Inter({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+const poppins = Poppins({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-poppins",
+});
 export const metadata: Metadata = {
   title: "pcshop",
   description: "A PC shop made with Next which sells computers parts",
@@ -13,7 +34,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={` bg-slate-950 flex flex-col min-h-screen relative ${inter.className} ${poppins.variable} ${roboto.variable}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 export default function Home() {
   return (
-    <div className=" w-full h-screen bg-slate-950">
-      <h1 className=" font-medium text-4xl text-rose-400">
-        This is main heading
+    <div className="flex gap-4 flex-col">
+      <h1 className="text-4xl text-rose-500">
+        This is main heading with default inter font
       </h1>
+      <h1 className="text-4xl text-rose-500 font-poppins">
+        this is poppins with next and tailwind
+      </h1>
+      <p className="font-roboto text-rose-500">
+        This is roboto with next and tailwind
+      </p>
     </div>
   );
 }
